9386955e01478236fb6b5c5fe945f8c87f9706b7
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-06-22 16:47:53 +0200

    add single-department

--- index.php
+++ index.php
@@ -1,10 +1,48 @@
 <?php
-// importo il database
+// importo il database e la classe departments
 require_once __DIR__ . "/database.php";
+require_once __DIR__ . "/Departments.php";
 
 // creo una query
 $sql = "SELECT * FROM departments";
 $result = $conn->query($sql);
-var_dump($result); //--> verifica
 
+$departments = [];
+// controllo se ci sono risultati
+if ($result && $result->num_rows > 0) {
+    //Proviamo ad accedere ai valori
+    while ($row = $result->fetch_assoc()) {
+      $department = new Departments($row["id"], $row["name"]);
+      $departments[] = $department;
+    }
+  } elseif ($result) {
+    // query è andata a buon fine ma non ci sono risultati
+  } else {
+    //query non andata a buon fine
+    echo "error";
+    die();
+}
 ?>
+
+<!-- stampo in html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h2>Lista Dipartimenti:</h2>
+    <ul>
+        <?php foreach($departments as $department) { ?>
+        <li><b><?php echo $department->name; ?></b></li>
+        <li>
+            <a href="single-department.php?id=<?php echo $department->id; ?>">Visualizza dettagli</a>
+        </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
---
+++ single-department.php
@@ -0,0 +1,57 @@
+<?php 
+
+// importo il database e la classe departments
+require_once __DIR__ . "/database.php";
+require_once __DIR__ . "/Departments.php";
+
+// inizializzo statement
+$stmt = $conn->prepare("SELECT * FROM `departments` WHERE `id`=?");
+$stmt->bind_param("d", $id);
+$id = $_GET["id"];
+
+// esecuzione
+$stmt->execute();
+$result = $stmt->get_result();
+
+$departments = [];
+
+if ($result && $result->num_rows > 0) {
+
+    while ($row = $result->fetch_assoc()) {
+      $current_department = new Departments($row["id"], $row["name"]);
+      $current_department->setContactData($row["address"], $row["phone"], $row["email"], $row["website"]);
+      $current_department->head_of_department = $row["head_of_department"];
+      $departments[] = $current_department;
+    }
+  } elseif ($result) {
+      echo "nessun dipartimento nel database";
+  } else {
+    //errore nella query
+    echo "errore";
+}
+?>
+
+<!-- stampo in html il singolo dipartimento -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <a href="index.php"> &larr; Torna ai Dipartimenti</a>
+    <?php foreach($departments as $department) { ?>
+        <h3><?php echo $department->name; ?></h3>
+        <p><b>Capo del Dipartimento:</b> <?php echo $department->head_of_department; ?></p>
+        <h4>Contatti:</h4>
+        <ul>
+            <?php foreach($department->getContactsAsArray() as $key => $value) { ?>
+                <li><small><?php echo "$key: $value"; ?></small></li> 
+            <?php } ?>
+        </ul>
+    <?php }?>
+</body>
+</html>
